f7413746fc8675c27dc89141d476ebd9e00ad1c6
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2018-04-04 22:56:45 +0300

    Added gzipped files support

--- example.py
+++ example.py
@@ -5,12 +5,19 @@ A simple example of using yack
 import os
 import click
 import yack
+import gzip
 from Bio import SeqIO
 from metrics import  rjsd
 
+
 def _get_sequences(filename):
     input_format = os.path.splitext(filename)[1][1:]
-    return [str(record.seq) for record in SeqIO.parse(filename, input_format)]
+    if input_format == "gz":
+        with gzip.open(filename, "rt") as handle:
+            input_format = os.path.splitext(os.path.splitext(filename)[0])[1][1:]
+            return [str(record.seq) for record in SeqIO.parse(handle, input_format)]
+    else:
+        return [str(record.seq) for record in SeqIO.parse(filename, input_format)]
 
 
 @click.command()
